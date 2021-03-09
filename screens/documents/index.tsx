910ca3caf6cb1efdf983e9024eb65c541f709b6a
Alexander Everaert <alexander.everaert@student.howest.be>
2021-03-09 08:53:54 +0100

    created page components

---
+++ screens/documents/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const Documents = function({ navigation }: any){
+    return(
+        <Text>Documents</Text>
+    );
+}
+
+export default Documents;
---
+++ screens/settings/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const Settings = function({ navigation }: any){
+    return(
+        <Text>Settings</Text>
+    );
+}
+
+export default Settings;
