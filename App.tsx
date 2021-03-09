8a1b71ce19328f5be27b6d8b75f9298897cf4b2f
Alexander Everaert <alexander.everaert@student.howest.be>
2021-03-09 08:54:05 +0100

    js to tsx

--- App.tsx
+++ App.tsx
@@ -3,11 +3,21 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Documents from './screens/documents';
+import Reader from './screens/reader';
+import Settings from './screens/settings/index';
 
 export default function App() {
+
+  const Tab = createBottomTabNavigator();
+
   return (
     <NavigationContainer>
-
+      <Tab.Navigator>
+        <Tab.Screen name="Documents" component = {Documents}/>
+        <Tab.Screen name="Reader" component = {Reader}/>
+        <Tab.Screen name="Settings" component = {Settings}/>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
