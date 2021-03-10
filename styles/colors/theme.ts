d34f90253426e1f828e714dd0c890ae673cdefc9
Alexander Everaert <alexander.everaert@student.howest.be>
2021-03-10 15:45:30 +0100

    styling

--- styles/colors/theme.ts
+++ styles/colors/theme.ts
@@ -1,19 +1,57 @@
 import { StyleSheet } from 'react-native';
-
-export const neutral = StyleSheet.create({
-    100: {
-        color: '#FFFFFF'
-    },
-    600: {
-        color: '#939393'
-    },
-    700: {
-        color: '#707070'
-    },
-    800: {
-        color: '#656565'
-    },
-    900: {
-        color: '#0D0D0D'
-    },
-});
+ 
+export const neutral = {
+    100: '#FFFFFF',
+    600: '#939393',
+    700: '#707070',
+    800: '#656565',
+    900: '#0D0D0D',
+};
+ 
+// background.neutral[800]
+ 
+export const background = {
+    neutral: StyleSheet.create({
+        100: {
+            backgroundColor: neutral[100]
+        },
+        600: {
+            backgroundColor: neutral[600]
+        },
+        700: {
+            backgroundColor: neutral[700]
+        },
+        800: {
+            backgroundColor: neutral[800]
+        },
+        900: {
+            backgroundColor: neutral[900]
+        },
+    }),
+    theme: {
+        // ...
+    }
+}
+ 
+export const text = {
+    neutral: StyleSheet.create({
+        100: {
+            color: neutral[100]
+        },
+        600: {
+            color: neutral[600]
+        },
+        700: {
+            color: neutral[700]
+        },
+        800: {
+            color: neutral[800]
+        },
+        900: {
+            color: neutral[900]
+        },
+    }),
+    theme: {
+        // ...
+    }
+}
--- styles/components/card.ts
+++ styles/components/card.ts
@@ -1,16 +1,58 @@
 
-import { StyleSheet } from 'react-native';
+import { Dimensions, StyleSheet } from 'react-native';
 
 export const card = StyleSheet.create({
     base: {
-        borderRadius: 8,
+        borderRadius: 10,
+        padding: 16
     },
 
+    holder: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-between',
+        marginHorizontal: 16,
+    },
+    
     small: {
+        width: (Dimensions.get('screen').width - (3 * 16)) / 2,
+        height: (Dimensions.get('screen').width - (3 * 16)) / 2,
+        backgroundColor: 'red',
+        marginBottom: 16,
 
+        justifyContent: 'center',
+        alignItems: 'center'
     },
 
     large: {
-
+        flex: 1,
+        marginHorizontal: 16,
+        borderBottomLeftRadius: 0,
+        borderBottomRightRadius: 0,
     },
+
+    addIcon :{
+        width: 50,
+        height: 50,
+    },
+
+    insertTitle: {
+        fontFamily: 'PlayfairDisplayBold',
+        fontSize: 24,
+        marginBottom: 0
+    },
+     
+    insertAuthor: {
+        fontFamily: 'PlayfairDisplay',
+        marginBottom: 8,
+        fontSize: 16
+    },
+
+    insertText: {
+        lineHeight: 20,
+        flex: 1,
+
+    }
+
+
 })
